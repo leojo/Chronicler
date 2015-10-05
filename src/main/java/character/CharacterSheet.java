7f1533162cde08eae182f8c992cdc51aa31128cf
BjornBjarnsteins <bjornbjarnst@gmail.com>
2015-10-05 23:32:52 +0000

    Refactored ability scores for consistency

--- src/main/java/character/CharacterSheet.java
+++ src/main/java/character/CharacterSheet.java
@@ -10,12 +10,72 @@ import java.util.stream.IntStream;
  */
 public class CharacterSheet {
 	// TODO: Make this enum
-	public final String[] abilities = {"STR", "DEX", "CON", "INT", "WIS", "CHA"};
+	public enum AbilityID {STR, DEX, CON, INT, WIS, CHA}
 	public enum SavingThrowID {FORT, REF, WILL}
+
+	class AbilityScore {
+		String name;
+		String shortName;
+
+		Map<String, Integer> bonuses; // Map<source, value> of bonuses to this skill
+		int totalValue;
+		int modifier;
+
+		public AbilityScore(AbilityID id) {
+			switch (id) {
+				case STR:
+					this.name = "Strength";
+					this.shortName = "STR";
+					break;
+				case DEX:
+					this.name = "Dexterity";
+					this.shortName = "DEX";
+					break;
+				case CON:
+					this.name = "Constitution";
+					this.shortName = "CON";
+					break;
+				case INT:
+					this.name = "Intelligence";
+					this.shortName = "INT";
+					break;
+				case WIS:
+					this.name = "Wisdom";
+					this.shortName = "WIS";
+					break;
+				case CHA:
+					this.name = "Charisma";
+					this.shortName = "CHA";
+					break;
+			}
+			this.totalValue = 0;
+			this.modifier = 0;
+			this.bonuses = new HashMap<>();
+			this.bonuses.put("Base Score", 10);
+		}
+
+		public void update(CharacterSheet character) {
+			this.totalValue = 0;
+			for (int v : this.bonuses.values()) {
+				this.totalValue += v;
+			}
+
+			this.modifier = (this.totalValue / 2) - 5;
+		}
+
+		@Override
+		public String toString() {
+			return "AbilityScore{" +
+					       "shortName='" + shortName + '\'' +
+					       ", totalValue=" + totalValue +
+					       ", bonuses=" + bonuses +
+					       '}';
+		}
+	}
 
 	class Skill {
 		String name;
-		String baseSkill;
+		AbilityID baseSkill;
 		int id;
 		boolean trainedOnly;
 		boolean armorPenalty;
@@ -27,7 +87,7 @@ public class CharacterSheet {
 			this.id = id;
 			// TODO: This should load the correct values from database
 			this.name = "Appraise";
-			this.baseSkill = "INT";
+			this.baseSkill = AbilityID.INT;
 			this.trainedOnly = false;
 			this.armorPenalty = false;
 
@@ -45,7 +105,7 @@ public class CharacterSheet {
 
 		@Override
 		public String toString() {
-			return "Skill{" +
+			return "Ability{" +
 					       "name='" + name + '\'' +
 					       '}';
 		}
@@ -53,7 +113,7 @@ public class CharacterSheet {
 
 	class SavingThrow {
 		String name;
-		String baseSkill;
+		AbilityID baseSkill;
 		Map<String, Integer> bonuses; // Map<source, value> of bonuses to this skill
 		int totalValue;
 
@@ -62,19 +122,20 @@ public class CharacterSheet {
 			switch (id) {
 				case FORT:
 					this.name = "Fortitude";
-					this.baseSkill = "CON";
+					this.baseSkill = AbilityID.CON;
 					break;
 				case REF:
 					this.name = "Reflex";
-					this.baseSkill = "DEX";
+					this.baseSkill = AbilityID.DEX;
 					break;
 				case WILL:
 					this.name = "Will";
-					this.baseSkill = "WIS";
+					this.baseSkill = AbilityID.WIS;
 					break;
 			}
 
 			this.bonuses = new HashMap<>();
+			// Probably take all this out and do this in the update function
 			this.bonuses.put("Base Save", 0);
 			this.bonuses.put("Ability Modifier", 0);
 		}
@@ -99,17 +160,17 @@ public class CharacterSheet {
 
 	public Map<Integer, Integer> classID; //Placeholders
 	public Integer raceID;
-    public Map<String, Integer> abilityScores;
-	public Vector<Skill> skills;
-	public Vector<Integer> featID;
-	public Vector<SavingThrow> savingThrows;
+    public Map<AbilityID, AbilityScore> abilityScores;
+	public Map<Integer, Skill> skills;
+	public Vector<Integer> featID; // TODO: Refactor to map for consistency
+	public Map<SavingThrowID, SavingThrow> savingThrows;
 
     public CharacterSheet() {
 	    this.resetAbilities();
 	    this.resetSkills();
 	    this.classID = new HashMap<>();
 	    this.featID = new Vector<>();
-	    this.savingThrows = new Vector<>();
+	    this.resetSavingThrows();
 	    this.raceID = null;
 	    this.update();
     }
@@ -124,44 +185,34 @@ public class CharacterSheet {
 	 */
 
 	public void update() {
-		this.calculateSavingThrowID();
-	}
-
-	public void calculateSavingThrowID() {
-		// getBaseSave();
-	}
-
-	public void calculateSavingThrow() {
-
+		this.abilityScores.values().stream().forEach(v -> v.update(this));
+		this.savingThrows.values().stream().forEach(v -> v.update(this));
 	}
 
 	/*
-	 * Utilities for ability score manipulation
+	 * Ability score
 	 */
 
 	public void resetAbilities() {
 	    this.abilityScores = new HashMap<>();
-		for (String name : abilities) {
-			this.abilityScores.put(name, 10);
+		for (AbilityID id : AbilityID.values()) {
+			this.abilityScores.put(id, new AbilityScore(id));
 		}
 	}
 
-    public void setAbility(String abilityName, int value) {
-        this.abilityScores.replace(abilityName, value);
-    }
+	/*
+	 * Saving throws
+	 */
 
-    public void incrementAbility(String abilityName) {
-	    // Increments ability abilityName by 1
-        this.abilityScores.computeIfPresent(abilityName, (x, oldValue) -> oldValue + 1);
-    }
-
-	public void decrementAbility(String abilityName) {
-		// Decrements ability abilityName by 1
-		this.abilityScores.computeIfPresent(abilityName, (x, oldValue) -> oldValue - 1);
+	public void resetSavingThrows() {
+		this.savingThrows = new HashMap<>();
+		for (SavingThrowID id: SavingThrowID.values()) {
+			this.savingThrows.put(id, new SavingThrow(id));
+		}
 	}
 
 	/*
-	 * Utilities for feat manipulation
+	 * Feats
 	 */
 
 	public void acquireFeat(int id) {
@@ -170,23 +221,22 @@ public class CharacterSheet {
 	}
 
 	/*
-	 * Utilities for skill manipulation
+	 * Skills
 	 */
 
 	public void resetSkills() {
-		this.skills = new Vector<>();
+		this.skills = new HashMap<>();
 		IntStream skillList = IntStream.range(1, 10); // TODO: Make these items load from database
 		skillList.forEach(skillID -> {
-			this.skills.add(new Skill(skillID));
+			this.skills.put(skillID, new Skill(skillID));
 		});
 	}
 
     public static void main(String[] args) {
         CharacterSheet c = new CharacterSheet();
-	    c.incrementAbility("STR");
-	    c.setAbility("INT", 14);
 	    c.acquireFeat(2);
-        System.out.println(c.abilityScores);
+        System.out.println(c.abilityScores.get(AbilityID.STR));
+	    System.out.println(c.savingThrows.get(SavingThrowID.FORT));
 	    System.out.println(c.featID);
 	    c.levelUp(1);
 	    c.levelUp(2);
