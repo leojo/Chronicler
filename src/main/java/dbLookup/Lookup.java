fd694a972dee558ef43dd26e9c886ea64f9ad10d
leojo <leojohannsson91@gmail.com>
2015-10-16 17:54:48 +0000

    Added an exact search option along with feat/spell/item searches

--- src/main/java/dbLookup/Lookup.java
+++ src/main/java/dbLookup/Lookup.java
@@ -25,27 +25,53 @@ public class Lookup {
         return c;
     }
 
-    public String searchClass(String className){
-        // TODO: Needs more work, this is just a prototype!
-        String query = "SELECT * FROM dnd_characterclass AS a JOIN dnd_characterclassvariant AS b " +
-                "ON a.id=b.character_class_id WHERE name='"+className+"';";
-        System.out.println("Searching for class: "+className+"\n Using SQL query :"+query);
-        ResultSet rs = search(query);
-        if(rs != null){
-            return rs2string(rs);
-        }
-        return null;
+    // TODO: These are not final!
+    public ResultSet playerClass(String searchTerm){
+        String[] field = {"name","short_description","class_features"}; // Priority order of search fields
+        String query_template = "SELECT * FROM dnd_characterclass AS a JOIN dnd_characterclassvariant AS b ON a.id=b.character_class_id " +
+                "WHERE advancement<>\"\" AND \"%2$s\" LIKE '%1$s';";
+        return searchByTemplate(query_template, field, searchTerm);
     }
 
     public ResultSet skill(String searchTerm){
-        // TODO: Needs more work, this is just a prototype!
         // This is the order in which we want to search through the fields of the skills
         String[] field = {"name","description","check","action","special","synergy","untrained"};
         String query_template = "SELECT * FROM dnd_skill AS a JOIN dnd_skillvariant AS b ON a.id = b.skill_id WHERE \"%2$s\" LIKE '%1$s';";
+        return searchByTemplate(query_template, field, searchTerm);
+    }
+
+    public ResultSet spell(String searchTerm){
+        // This is the order in which we want to search through the fields of the skills
+        String[] field = {"name","description"};
+        String query_template = "SELECT * FROM dnd_spell WHERE \"%2$s\" LIKE '%1$s';";
+        return searchByTemplate(query_template, field, searchTerm);
+    }
+
+    public ResultSet feat(String searchTerm){
+        // This is the order in which we want to search through the fields of the skills
+        String[] field = {"name","benefit","description","normal"};
+        String query_template = "SELECT * FROM dnd_feat WHERE \"%2$s\" LIKE '%1$s';";
+        return searchByTemplate(query_template, field, searchTerm);
+    }
+
+    public ResultSet item(String searchTerm){
+        // This is the order in which we want to search through the fields of the skills
+        String[] field = {"name","description"};
+        String query_template = "SELECT * FROM dnd_item WHERE \"%2$s\" LIKE '%1$s';";
+        return searchByTemplate(query_template, field, searchTerm);
+    }
+
+    // Search the database using s query template where %2$s will be replaced with the field and %1$s will be replaced with the search term
+    // if the search term ends with a recognized /[command] it will be implemented, otherwize discarded.
+    private ResultSet searchByTemplate(String query_template, String[] field, String searchTerm){
         ResultSet rs = null;
+        String term = "";
+        if(searchTerm.toLowerCase().endsWith("/exact")) term = searchTerm.replace('*','%').split("/")[0];
+        else term = "%"+searchTerm.replace('*','%').split("/")[0]+"%"; // % is the SQL wildcard
+        System.out.println("Searching for term "+term);
         int i = 0;
         while(rs == null && i<field.length) {
-            String query = String.format(query_template, searchTerm.replace('*','%'), field[i]);
+            String query = String.format(query_template, term, field[i]);
             rs = search(query);
             i++;
         }
@@ -125,18 +151,18 @@ public class Lookup {
         Scanner scan = new Scanner(System.in);
         Boolean b = true;
         while(b){
-            System.out.println("\n\nSearch for skill: ");
+            System.out.println("\n\nSearch for class: ");
             String searchString = scan.nextLine();
             if(searchString.equalsIgnoreCase("exit")) b = false;
             else {
-                ResultSet sr = find.skill(searchString);
+                ResultSet sr = find.playerClass(searchString);
                 try {
-                    System.out.println("\nFound skill(s): ");
+                    System.out.println("\nFound class(es): ");
                     while (sr.next()) {
                         System.out.println(sr.getString("name"));
                     }
                 } catch (Exception e) {
-                    System.err.println("Error in main: " + e.getMessage());
+                    // Suppress exceptions
                 }
             }
         }
