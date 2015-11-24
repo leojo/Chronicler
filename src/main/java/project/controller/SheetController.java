91f88d3783e0f677f81aac026e148437981a239f
andreabjork <andreabjork43@gmail.com>
2015-11-24 02:12:56 +0000

    Working on making character sheet compatible with our bean

--- src/main/java/project/controller/SheetController.java
+++ src/main/java/project/controller/SheetController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import project.service.account.User;
 import project.service.character.CharacterBean;
+import project.service.character.CharacterSheet;
 import project.service.dbLookup.Lookup;
 
 import javax.servlet.http.HttpSession;
@@ -46,14 +47,18 @@ public class SheetController {
             // Make sure user is who we think they are, and put character bean in our model.
             User user = (User)session.getAttribute("userId");
             model.addAttribute("user", user);
-            model.addAttribute("character", charbean);
 
-            if(user.getUserID().equals("andrea"))
+
+            if(!charbean.getClassName().equals("") && !charbean.getRace().equals("")) {
                 // TODO : Initialize character sheet from bean
                 // TODO : Get bean from character sheet and update bean
-
+                CharacterSheet cs = new CharacterSheet(charbean, true);
+                //charbean = cs.getBean();
+                model.addAttribute("character", charbean);
+                //session.setAttribute("characterSheet", cs);
+                //cs = session.getAttribute("characterSheet");
                 return "characterSheet";
-            else
+            } else
                 return "loginFail";
         }
 
