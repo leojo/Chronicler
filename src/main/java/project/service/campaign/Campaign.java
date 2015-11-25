92a0f7ec90fc47ed123dca9fe353ec9b97ab4dec
BjornBjarnsteins <bjb46@hi.is>
2015-11-25 15:20:55 +0000

    campaign database stuff

--- src/main/java/project/service/campaign/Campaign.java
+++ src/main/java/project/service/campaign/Campaign.java
@@ -7,14 +7,14 @@ import java.util.Map;
 import java.util.Vector;
 
 /**
- * Created by whiskeyjack on 10/28/15.
+ * Created by bjornbjarnst on 10/28/15.
  */
 public class Campaign {
-	public String name;
-	public Vector<User> players;
-	public Vector<User> DMs;
-	public Map<String, String> imagePaths; // Map<[Image title], [Image path]>
-	public String notes;
+	private String name;
+	private Vector<User> players;
+	private Vector<User> DMs;
+	private Map<String, String> imagePaths; // Map<[Image title], [Image path]>
+	private String notes;
 
 	public Campaign(String name) {
 		this.name = name;
@@ -38,5 +38,45 @@ public class Campaign {
 
 	public void addImage(String name, String path) {
 		this.imagePaths.put(name, path);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Vector<User> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(Vector<User> players) {
+		this.players = players;
+	}
+
+	public Vector<User> getDMs() {
+		return DMs;
+	}
+
+	public void setDMs(Vector<User> DMs) {
+		this.DMs = DMs;
+	}
+
+	public Map<String, String> getImagePaths() {
+		return imagePaths;
+	}
+
+	public void setImagePaths(Map<String, String> imagePaths) {
+		this.imagePaths = imagePaths;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
 	}
 }
