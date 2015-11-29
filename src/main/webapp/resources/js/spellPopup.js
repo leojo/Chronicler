7d4e61633812d11bf01752c713eda0bcf8ff90d8
andreabjork <andreabjork43@gmail.com>
2015-11-29 20:46:17 +0000

    Spell list should be interactive

--- src/main/webapp/resources/js/spellPopup.js
+++ src/main/webapp/resources/js/spellPopup.js
@@ -29,17 +29,17 @@ $(function() {
 
 
 				// Hide spans and show editable fields in their place
-				$resetBtn.click(function (e) {
-					$spellSlot.removeClass('available');
-					$spellSlot.removeClass('spent');
+				$spellSlot.click(function (e) {
 					if(!spellOfferOpen) {
 						$spellOffer.show();
 						spellOfferOpen = true;
 						$spells.click(function() {
 							$spellID = $(this).attr('spell_id');
+							console.log("does spell ID look like a jquery object");
+
 							$spellSlotInput.val($spellID);
 							$spellSlot.find('span').replaceWith($(this).html());
-							$spellSlot.addClass('available');
+							$spellSlot.removeClass('unprepared');
 							$spellSlot.makeSpendable();
 							$('.spellOffer').hide();
 							spellOfferOpen  = false;
@@ -53,28 +53,49 @@ $(function() {
 		},
 
 		makeSpendable: function() {
-			var $spellSlot = $(this);
-			var $spellSlotStatus = $spellSlot.find('.spellSlotStatus');
-			console.log($spellSlotStatus);
-			var submitSpend = function () {
-				if ($spellSlotStatus.val() !== '') {
-					console.log("submitting form!");
-					$('#sheetForm').ajaxSubmit({url: 'updateSpellslot', type: 'post'});
-					$spellSlot.unbind('click');
-
-				}
-			};
-
-			$spellSlot.click(function() {
-				$(this).removeClass('available');
-				$(this).addClass('spent');
-				console.log("This is the spellSlotStatus we're about to change");
+			$(this).each(function() {
+				var $spellSlot = $(this);
+				var $spellSlotStatus = $spellSlot.find('.spellSlotStatus');
 				console.log($spellSlotStatus);
-				console.log("Value before"+$spellSlotStatus.val());
-				$spellSlotStatus.val('spent');
-				console.log("Value after"+$spellSlotStatus.val());
-				console.log($spellSlotStatus)
-				submitSpend();
+				var submitSpend = function () {
+					if ($spellSlotStatus.val() !== '') {
+						console.log("submitting form!");
+						$('#sheetForm').ajaxSubmit({url: 'updateSpellslot', type: 'post'});
+						$spellSlot.unbind('click');
+
+					}
+				};
+
+				$spellSlot.click(function() {
+					$(this).removeClass('available');
+					$(this).addClass('spent');
+					console.log("This is the spellSlotStatus we're about to change");
+					console.log($spellSlotStatus);
+					console.log("Value before"+$spellSlotStatus.val());
+					$spellSlotStatus.val('spent');
+					console.log("Value after"+$spellSlotStatus.val());
+					console.log($spellSlotStatus)
+					submitSpend();
+				});
+			});
+
+		},
+
+		makeResetable: function() {
+			$(this).each(function() {
+				var $spellSlot = $(this);
+				$spellSlot.find('.spellResetBtn').click(function(e) {
+					console.log("HITTING RESET BUTTON");
+					e.stopPropagation();
+					console.log("this is the spellslot");
+					console.log($spellSlot);
+					console.log("and its html");
+					console.log($spellSlot.html());
+					$spellSlot.find('span').replaceWith('<span class="content unprepped"><p class="spellName">Unprepared spell</p><p class="spellDescr">Click to select a spell</p></span>');
+					console.log($spellSlot.html());
+					$spellSlot.addClass('unprepared');
+					$spellSlot.makeSpellOffer();
+				});
 			});
 		},
 
@@ -84,8 +105,11 @@ $(function() {
 		}
 	});
 	//$('.available').parent().parent().addClass('available');
- 	$('.spellSlot').makeSpellOffer();
+ 	$('.unprepped').parent().parent().makeSpellOffer();
 	$('.available').makeSpendable();
+	$('.spellSlot').makeResetable();
+
+
 });
 
 
