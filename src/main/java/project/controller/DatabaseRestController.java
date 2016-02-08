fcad70298b70609e914260bc28fd22213979367d
andreabjork <andreabjork43@gmail.com>
2016-02-08 22:43:48 +0000

    Database controller edit 2

--- src/main/java/project/controller/DatabaseRestController.java
+++ src/main/java/project/controller/DatabaseRestController.java
@@ -1,28 +1,33 @@
 package project.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by andrea on 8.2.2016.
  */
 @RestController
-@RequestMapping("getString")
 public class DatabaseRestController {
 
-    @RequestMapping(value = "/{thisString}", method = RequestMethod.GET, produces = "application/json")
-    public String StringResponse(@PathVariable String thisString) {
-        return thisString;
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
     }
 
-    /*
-    public class response{
-        private final String s;
 
-        public response(String string){
-            s = string;
+    public class Greeting {
+        private final long id;
+        private final String content;
+        public Greeting(long id, String content) {
+            this.id = id;
+            this.content = content;
         }
-    }*/
+        public long getId() { return id; }
+        public String getContent() { return content; }
+    }
 }
