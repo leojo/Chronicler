4e37626d7874aff21b1f32faf14c4e41097a6722
leojo <leojohannsson91@gmail.com>
2016-04-17 14:43:51 +0000

    Added option to controller to update existing charactersheet

--- src/main/java/project/controller/DatabaseRestController.java
+++ src/main/java/project/controller/DatabaseRestController.java
@@ -142,7 +142,7 @@ public class DatabaseRestController {
     // Will only respond to a request with a cookie that matches a cookie for some user
     // in the database.
     @RequestMapping(value = "/storeChar", method = RequestMethod.POST)
-    public Response storeChar(HttpServletRequest req){
+    public Response storeChar(HttpServletRequest req, @RequestParam("new") boolean newChar,@RequestParam("id") int charID){
         String userID = userIdFromCookie(req.getHeader("Cookie"));
         if(userID == null) return new Response("failure", "User needs to login");
 
@@ -175,8 +175,10 @@ public class DatabaseRestController {
 
             AccountStorage storage = new AccountStorage("data/userAccounts.sqlite");
             // Store the character information in the database:
-            int charID = storage.getNextID("Characters");
-            int res = storage.addCharacterJSON(userID,charJSON,character.getString("name"));
+            if(newChar) charID = storage.getNextID("Characters");
+            int res;
+            if(newChar) res = storage.addCharacterJSON(userID,charJSON,character.getString("name"));
+            else res = storage.updateCharacterJSON(userID, charID, charJSON, character.getString("name"));
             System.out.println("Return message from updateRaw is "+res + " after adding "+character.getString("name")+" for user "+userID+" with JSON "+charJSON);
             if(res==1) return new Response("success", ""+charID);
             else return new Response("failure", "Failed to execute SQL insert statement");
